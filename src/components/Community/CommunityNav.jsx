3def0616f9d27c23de62e21b25ff626346728bb9
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-07 13:49:59 -0500

    Add navbar

--- src/components/Community/CommunityNav.jsx
+++ src/components/Community/CommunityNav.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const CommunityNav = () => {
   return (
-    <nav className="absolute top-[239px] w-full h-[43px]">
-      <ul className="flex gap-10 items-center h-full font-bold text-gray-100">
-        <li className="ml-[32px]">Resume</li>
-        <li>Logros</li>
-        <li>Capturas</li>
-        <li>Amigos</li>
-        <li>Publicaciones</li>
+    <nav className="absolute top-[239px] w-full h-[43px] 2xl:top-[40%]">
+      <ul className="flex gap-10 items-center h-full font-bold text-gray-100 cursor-pointer">
+        <li className="ml-[32px] 2xl:ml-[8%]">Resume</li>
+        <li>Achievements</li>
+        <li>Captures</li>
+        <li>Friends</li>
+        <li>Publications</li>
       </ul>
     </nav>
   )
