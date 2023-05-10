15ef5e5935080000f7f28343859913a3f52d6d56
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-09 23:06:06 -0500

    Add basic captures

--- src/components/Community/CommunityCaptures.jsx
+++ src/components/Community/CommunityCaptures.jsx
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from 'react'
-
+import React from 'react'
+import { capturesArray } from '../../assets/communityPage'
 
 const CommunityCaptures = () => {
+  console.log(capturesArray)
+  return (
+    <div className="grid grid-cols-[30%_minmax(70%,_1fr)] gap-[20px]">
+      {capturesArray.map((capture, index) => (
+        <img src={capture} key={index} />
+      ))}
+    </div>
 
+  )
 }
 
 export default CommunityCaptures
